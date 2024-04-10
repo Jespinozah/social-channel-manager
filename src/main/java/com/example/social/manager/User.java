b5cc352ffe41c7e3bd0467a6e8e6e0efd7f1afc9
Brayan Espinoza <bjespinozah@gmail.com>
2024-04-10 15:02:36 -0500

    Add lombok library

--- src/main/java/com/example/social/manager/User.java
+++ src/main/java/com/example/social/manager/User.java
@@ -4,38 +4,21 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Entity // This tells Hibernate to make a table out of this class
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
-
-	private String name;
-
+	private String firstName;
+	private String lastName;
 	private String email;
+	private String password;
 
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
 }
